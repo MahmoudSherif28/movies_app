97f209b12dc5b642358c6611514fe0b4b14c5790
mahmoud sherif <mahmsherif28@gmail.com>
2024-02-24 22:55:14 +0200

    added home screen

---
+++ lib/core/CustomMovieCard.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movies/features/Home/Data/models/movie_model.dart';
+
+class CustomMovieCard extends StatelessWidget {
+  const CustomMovieCard({
+    super.key,
+    required this.movieModel,
+  });
+  final MovieModel movieModel;
+  @override
+
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          height: 175.h,
+          width:
+          MediaQuery.of(context).size.width * 0.4,
+          child: Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  movieModel.image,
+                  height: 175.h,
+                  width: MediaQuery.of(context)
+                      .size
+                      .width *
+                      0.4,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                top: 4,
+                right: 1,
+                child: Container(
+                  height: 36.h,
+                  width: 36.w,
+                  child: Padding(
+                    padding: const EdgeInsets.all(4),
+                    child: Icon(
+                      Icons.bookmark_outline_outlined,
+                      color: Colors.white54,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                movieModel.title,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold ,
+                    color: Colors.white54),
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Text(
+                    movieModel.rating,
+                    style: TextStyle(
+                        color: Colors.white54,
+                        fontWeight: FontWeight.w900),
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: Color(0xff937900),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/Home/Presentation/views/home_screen.dart
+++ lib/features/Home/Presentation/views/home_screen.dart
@@ -4,9 +4,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movies/core/CustomMovieCard.dart';
 import 'package:movies/features/Home/Data/repo/home_repo.dart';
-import 'package:movies/features/Home/Presentation/views/home_screen.dart';
-import 'package:movies/features/Home/Presentation/widgets/CustomProductCard.dart';
 
 class homescreen extends StatelessWidget {
   homescreen({super.key});
@@ -72,80 +71,8 @@ class homescreen extends StatelessWidget {
                                   crossAxisCount: 2),
                           itemCount: snapshot.data!.length,
                           itemBuilder: (context, index) {
-                            return Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Container(
-                                  height: 175.h,
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.4,
-                                  child: Stack(
-                                    children: [
-                                      ClipRRect(
-                                        borderRadius: BorderRadius.circular(10),
-                                        child: Image.network(
-                                          snapshot.data![index].image,
-                                          height: 175.h,
-                                          width: MediaQuery.of(context)
-                                                  .size
-                                                  .width *
-                                              0.4,
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                      Positioned(
-                                        top: 4,
-                                        right: 1,
-                                        child: Container(
-                                          height: 36.h,
-                                          width: 36.w,
-                                          child: Padding(
-                                            padding: const EdgeInsets.all(4),
-                                            child: Icon(
-                                              Icons.bookmark_outline_outlined,
-                                              color: Colors.white54,
-                                            ),
-                                          ),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    children: [
-                                      Text(
-                                        textAlign: TextAlign.center,
-                                        overflow: TextOverflow.ellipsis,
-                                        snapshot.data![index].title,
-                                        style: TextStyle(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.bold ,
-                                            color: Colors.white54),
-                                      ),
-                                      Row(
-                                        children: [
-                                          SizedBox(
-                                            width: 50,
-                                          ),
-                                          Text(
-                                            snapshot.data![index].rating,
-                                            style: TextStyle(
-                                                color: Colors.white54,
-                                                fontWeight: FontWeight.w900),
-                                          ),
-                                          Icon(
-                                            Icons.star,
-                                            color: Color(0xff937900),
-                                          )
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            );
+                            return CustomMovieCard(movieModel:snapshot.data![index]
+                                );
                           });
                     }
                     return const Center(child: CircularProgressIndicator());
@@ -159,3 +86,4 @@ class homescreen extends StatelessWidget {
     );
   }
 }
+
